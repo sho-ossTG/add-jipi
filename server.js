4e9672f8c75240efa6b0b2e0164666b67c3a1e48
sho-ossTG <shovalhagni@gmail.com>
2026-02-04 10:48:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 const addonInterface = require("./addon");
 
-const port = Number(process.env.PORT || 7000);
-
-serveHTTP(addonInterface, { port });
+serveHTTP(addonInterface);
